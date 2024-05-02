06c661ffd3efb58e86efcdecdd78ab297aea47e5
Chahana Bhandari <chahanabhandari98@gmail.com>
2024-05-02 15:40:09 +0545

    assignement 3 startup

---
+++ Vidhyalaya/Data/VidhyalayaDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+public class VidhyalayaDbContext: DbContext
+{
+    
+    public DbSet<Grade> Grades { get; set; }
+    public DbSet<Student> Students { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Grade>().HasKey(g => g.Label);
+    }
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Vidhyalaya.db");
+    }
+}
