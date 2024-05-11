64935d315afa0034f86b990198469ac35b1f65d1
Chahana Bhandari <chahanabhandari98@gmail.com>
2024-05-11 08:54:24 +0545

    foreign key is added in Guardian class

---
+++ Vidhyalaya/Migrations/20240511021455_FKGuardian.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidhyalaya.Migrations
+{
+    /// <inheritdoc />
+    public partial class FKGuardian : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Guardians",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guardians_StudentId",
+                table: "Guardians",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Guardians_Students_StudentId",
+                table: "Guardians",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Guardians_Students_StudentId",
+                table: "Guardians");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Guardians_StudentId",
+                table: "Guardians");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Guardians");
+        }
+    }
+}
